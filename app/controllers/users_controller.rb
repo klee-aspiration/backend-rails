d23094d8dd9826b7845bce212c514026c08c9f50
Kang-Kyu Lee <klee@aspiration.com>
2022-03-22 23:54:24 -0700

    'User' for a form and use rack-cors to allow localhost:3000

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,22 @@
+class UsersController < ApplicationController
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      render json: @user, status: :created
+    else
+      render json: @user.errors, status: :unprocessable_entity
+    end
+  end
+
+  def index
+    @users = User.all
+    render json: @users
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email)
+  end
+end
